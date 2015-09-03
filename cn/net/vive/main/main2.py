98777868775b96a2abec3b785284d54d4e172ad5
jinghewang <jinghewang@163.com>
2015-09-04 06:11:49 +0800

    add package

--- cn/net/vive/main/main2.py
+++ cn/net/vive/main/main2.py
@@ -9,24 +9,31 @@ __author__ = 'robin'
 # del a
 # print dir()
 
-import cn.net.vive.test3.using_list as ulist
-print ulist.getlen()
-ulist.listout()
-print('first is:',ulist.getfirst())
-print('last is:',ulist.getlast())
-ulist.sort()
-ulist.listout()
+# import cn.net.vive.test3.using_list as ulist
+# print ulist.getlen()
+# ulist.listout()
+# print('first is:',ulist.getfirst())
+# print('last is:',ulist.getlast())
+# ulist.sort()
+# ulist.listout()
+#
+# ulist.append("qqqq")
+# ulist.listout()
+#
+# ulist.dividing()
+# ulist.insert(1,'insert')
+# ulist.listout()
+#
+# ulist.dividing()
+# ulist.delfirst()
+# print('first is:',ulist.getfirst())
+#
 
-ulist.append("qqqq")
-ulist.listout()
 
-ulist.dividing()
-ulist.insert(1,'insert')
-ulist.listout()
 
-ulist.dividing()
-ulist.delfirst()
-print('first is:',ulist.getfirst())
+
+
+
 
 
 
